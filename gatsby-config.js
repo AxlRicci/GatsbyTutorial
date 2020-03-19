c3e31ca71bb85d28bb1b9f720c3b79b13114946f
AxlRicci <alexbricci@gmail.com>
2020-03-19 14:26:02 -0400

    added dynamic data from siteMetadata to header and footer using graphQL

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'Alex Ricci'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ]
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
 const Footer = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                    author
+                }
+            }
+        }
+    `)
     return (
         <footer>
-            <p>Created by Alex Ricci, copyright 2020</p>
+            <p>Created by {data.site.siteMetadata.author}, copyright 2020</p>
             
         </footer>
     )
