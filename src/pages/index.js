c4ff8c9e49cc989685fe59d91e9be3f8f407f8d8
AxlRicci <alexbricci@gmail.com>
2020-03-20 21:19:52 -0400

    changed home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,6 @@ const IndexPage = () => {
             <Head title="Home"/>
             <h1>Hello</h1>
             <h2>I'm Alex a coder in training.</h2>
-            <p>Need a dev? <Link to="/contact">Contact me</Link></p>
         </Layout>
     )
 }
